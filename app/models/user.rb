1f2a01b4c865b4e22c29872a764b2c3d7c7c38ed
Grady L Shelton <gradyish@gmail.com>
2017-07-14 13:01:22 -0700

    pass unit testing for user attribute presence and relationship to orders

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :orders
+  validates :name, :email, :password, :password_confirmation, presence: true
 
   def previous_orders
     self.orders.where(status: 2).includes(order_items: :product)
