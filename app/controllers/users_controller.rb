c3e7fb57958906c82a1311b22762f196274a04e5
Grady L Shelton <gradyish@gmail.com>
2017-07-14 15:57:21 -0700

    include password and email validations for signing up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
       flash[:notice] = user.name + ", you successfully signed up!"
       redirect_to '/'
     else
-      redirect_to '/signup'
+      flash[:alert] = 'Something went wrong. Passwords must be 6-20 characters, contain: at least one lowercase letter, at least one uppercase letter, and at least one numeric digit. Emails must be valid.'
+      redirect_to '/sign_up'
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :orders
-  validates :name, :email, :password, :password_confirmation, presence: true
+  validates :name, :email, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, :password_confirmation, presence: true, length: { minimum: 6}
+  validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+  validates_format_of :password, :with => /\A(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}\z/
 
   def previous_orders
     self.orders.where(status: 2).includes(order_items: :product)
